11940a5477f96caf94b67026a572f5f29cf4c62c
ebxn <ebengtuff@gmail.com>
2019-02-11 12:35:33 +0000

    basic navbar added

---
+++ src/components/navbar/Navbar.vue
@@ -0,0 +1,54 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="container">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <img class="image is-48x48"
+               :src="image"
+               width="112"
+               height="28">
+        </a>
+
+        <a role="button" class="navbar-burger burger"
+        aria-label="menu" aria-expanded="false" data-target="navbar">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbar" class="navbar-menu">
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <a class="button is-black">
+                <strong>Sign up</strong>
+              </a>
+              <a class="button is-light">
+                Log in
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+</template>
+
+<script>
+import brand from '@/assets/logo.png';
+
+export default {
+  name: '',
+  data() {
+    return {
+      image: brand,
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <Navbar />
+  </div>
 </template>
 
 <script>
+import Navbar from '@/components/navbar/Navbar.vue';
+
 export default {
   name: 'home',
+  components: {
+    Navbar,
+  },
 };
 </script>
 
